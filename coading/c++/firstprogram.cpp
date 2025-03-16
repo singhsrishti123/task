bea978b2695c9c302293efb41d28b1599931e90a
srishi7562 <srishti119988@gmail.com>
2025-03-16 20:17:38 +0530

    myfirsttask

---
+++ coading/c++/firstprogram.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    int age;
+    cout<<"what is your name? ";
+    cin >> name;
+    cout<<"how old are you? " ;
+    cin >> age;
+    return 0;
+}
